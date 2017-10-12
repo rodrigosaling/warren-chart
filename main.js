918e53b8fdc97c8ce9724201bd45cad4a294a186
Rodrigo Saling <rodrigosaling@gmail.com>
2017-10-12 20:39:15 -0300

    Lot of changes on chart and CDI display

--- main.js
+++ main.js
@@ -14,18 +14,17 @@ for (var i = 0; i < warrenFundPerformance.chart.label.length; i++) {
 
 
 for (var i = 0; i < warrenFundPerformance.chart.label.length; i++) {
+  cdi += +(cdiPerformance[i]);
   chartData2.push({
     date: warrenFundPerformance.chart.label[i],
-    // https://stackoverflow.com/a/12830454/785985
+    // +().toFixed() from https://stackoverflow.com/a/12830454/785985
     FWRF1: +(warrenFundPerformance.chart.data.FWRF1[i]*100),
     FWMM1: +(warrenFundPerformance.chart.data.FWMM1[i]*100),
     FWMM2: +(warrenFundPerformance.chart.data.FWMM2[i]*100),
     FWMM3: +(warrenFundPerformance.chart.data.FWMM3[i]*100),
     FWMM4: +(warrenFundPerformance.chart.data.FWMM4[i]*100),
-    CDI: +(cdi)
+    CDI: cdi
   });
-  // cdi += 0.031059;
-  cdi += cdiPerformance[i];
 }
 
 var chart2 = AmCharts.makeChart( 'chartdiv2', {
@@ -50,56 +49,76 @@ var chart2 = AmCharts.makeChart( 'chartdiv2', {
   },
   "legend": {
     "enabled": true,
-    "useGraphSettings": true
+    "useGraphSettings": true,
+    // data: [
+    //   {title: 'RF 1'},
+    //   {title: 'MM 1'},
+    //   {title: 'MM 2'},
+    //   {title: 'MM 3'},
+    //   {title: 'MM 4'},
+    //   {title: 'CDI'}
+    // ]
   },
   "graphs": [
     {
       "bullet": "round",
       "id": "AmGraph-1",
-      "title": "graph 1",
+      "title": "RF1",
       "valueField": "FWRF1",
       'bulletSize': 5,
-      'hideBulletsCount': 50
+      'hideBulletsCount': 50,
+      color: '#ee2e5d',
+      precision: 2
     },
     {
       "bullet": "round",
       "id": "AmGraph-2",
-      "title": "graph 2",
+      "title": "MM1",
       "valueField": "FWMM1",
       'bulletSize': 5,
-      'hideBulletsCount': 50
+      'hideBulletsCount': 50,
+      color: '#ff9001',
+      precision: 2
     },
     {
       "bullet": "round",
       "id": "AmGraph-3",
-      "title": "graph 3",
+      "title": "MM2",
       "valueField": "FWMM2",
       'bulletSize': 5,
-      'hideBulletsCount': 50
+      'hideBulletsCount': 50,
+      color: '#00b563',
+      precision: 2
     },
     {
       "bullet": "round",
       "id": "AmGraph-4",
-      "title": "graph 4",
+      "title": "MM3",
       "valueField": "FWMM3",
       'bulletSize': 5,
-      'hideBulletsCount': 50
+      'hideBulletsCount': 50,
+      color: '#0581ff',
+      precision: 2
     },
     {
       "bullet": "round",
       "id": "AmGraph-5",
-      "title": "graph 5",
+      "title": "MM4",
       "valueField": "FWMM4",
       'bulletSize': 5,
-      'hideBulletsCount': 50
+      'hideBulletsCount': 50,
+      color: '#6960da',
+      precision: 2
     },
     {
       "bullet": "round",
       "id": "AmGraph-6",
-      "title": "graph 6",
+      "title": "CDI",
       "valueField": "CDI",
       'bulletSize': 5,
-      'hideBulletsCount': 50
+      'hideBulletsCount': 50,
+      color: '#000000',
+      precision: 2
     }
   ]
 });
