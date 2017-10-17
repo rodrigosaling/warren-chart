d3ea9a0f33938b17f26018b1f983a6dbae1ee31d
Rodrigo Saling <rodrigosaling@gmail.com>
2017-10-17 00:01:53 -0200

    In progress to make the chart work on any date.

--- main.js
+++ main.js
@@ -65,145 +65,171 @@ Number.prototype.truncate = function(digits) {
 };
 //-----------------------------
 
-var chartData2 = [];
-var cdi = 1;
-var cdiPerformance = [];
-// var strint = strint();
-
-// Build the CDI array
-for (var i = 0; i < warrenFundPerformance.chart.label.length; i++) {
-  var monthlyValue = _.findLast(cdiValueChanges, function(item) {
-    return item.date <= warrenFundPerformance.chart.label[i];
-  });
-  // http://minhaseconomias.com.br/blog/investimentos/como-calcular-o-rendimento-de-seu-investimento-em-de-cdi
-  // http://estatisticas.cetip.com.br/astec/di_documentos/metodologia2_i1.htm
-  cdiPerformance.push(
-    (1 + (Math.pow((1+monthlyValue.value/100), 1/252)-1) * 100/100).truncate(16)
-  );
-}
-
-cdi = cdiPerformance[0];
-
-for (var i = 0; i < warrenFundPerformance.chart.label.length; i++) {
-
-  chartData2.push({
-    date: warrenFundPerformance.chart.label[i],
-    // +().toFixed() from https://stackoverflow.com/a/12830454/785985
-    FWRF1: +(warrenFundPerformance.chart.data.FWRF1[i]*100),
-    FWMM1: +(warrenFundPerformance.chart.data.FWMM1[i]*100),
-    FWMM2: +(warrenFundPerformance.chart.data.FWMM2[i]*100),
-    FWMM3: +(warrenFundPerformance.chart.data.FWMM3[i]*100),
-    FWMM4: +(warrenFundPerformance.chart.data.FWMM4[i]*100),
-    CDI: (((Math.ceil10(cdi,-8)-1).truncate(8))*100)
-  });
-  if (i < warrenFundPerformance.chart.label.length-1) {
-    // By using ceil and the -9 parameter, we get the most approximate values yet
-    cdi = Math.ceil10(cdi * (cdiPerformance[i + 1]), -9);
-  }
-}
-
-
-
-var chart = AmCharts.makeChart( 'chartdiv', {
-  type: 'serial',
-  dataProvider: chartData2,
-  "categoryField": "date",
-  "dataDateFormat": "YYYY-MM-DD",
-  "chartScrollbar": {
-    "enabled": true
-  },
-  'mouseWheelZoomEnabled':true,
-  'chartCursor': {
-    "enabled": true
-    // 'pan': true,
-    // 'valueLineEnabled': true,
-    // 'valueLineBalloonEnabled': true,
-    // 'cursorAlpha':1,
-    // 'cursorColor':'#258cbb',
-    // 'limitToGraph':'g1',
-    // 'valueLineAlpha':0.2,
-    // 'valueZoomable':true
-  },
-  "legend": {
-    "enabled": true,
-    "useGraphSettings": true,
-    // data: [
-    //   {title: 'RF 1'},
-    //   {title: 'MM 1'},
-    //   {title: 'MM 2'},
-    //   {title: 'MM 3'},
-    //   {title: 'MM 4'},
-    //   {title: 'CDI'}
-    // ]
-  },
-  "graphs": [
-    {
-      "bullet": "round",
-      "id": "AmGraph-1",
-      "title": "RF1",
-      "valueField": "FWRF1",
-      'bulletSize': 5,
-      'hideBulletsCount': 50,
-      lineColor: '#ee2e5d',
-      precision: 2,
-      lineThickness: 2
-    },
-    {
-      "bullet": "round",
-      "id": "AmGraph-2",
-      "title": "MM1",
-      "valueField": "FWMM1",
-      'bulletSize': 5,
-      'hideBulletsCount': 50,
-      lineColor: '#ff9001',
-      precision: 2,
-      lineThickness: 2
-    },
-    {
-      "bullet": "round",
-      "id": "AmGraph-3",
-      "title": "MM2",
-      "valueField": "FWMM2",
-      'bulletSize': 5,
-      'hideBulletsCount': 50,
-      lineColor: '#00b563',
-      precision: 2,
-      lineThickness: 2
-    },
-    {
-      "bullet": "round",
-      "id": "AmGraph-4",
-      "title": "MM3",
-      "valueField": "FWMM3",
-      'bulletSize': 5,
-      'hideBulletsCount': 50,
-      lineColor: '#0581ff',
-      precision: 2,
-      lineThickness: 2
-    },
-    {
-      "bullet": "round",
-      "id": "AmGraph-5",
-      "title": "MM4",
-      "valueField": "FWMM4",
-      'bulletSize': 5,
-      'hideBulletsCount': 50,
-      lineColor: '#6960da',
-      precision: 2,
-      lineThickness: 2
-    },
-    {
-      "bullet": "round",
-      "id": "AmGraph-6",
-      "title": "CDI",
-      "valueField": "CDI",
-      'bulletSize': 5,
-      'hideBulletsCount': 50,
-      lineColor: '#000000',
-      precision: 6,
-      lineThickness: 2
-    }
-  ]
+$('#updateChart').on('click', function() {
+  drawChart();
 });
 
+function drawChart() {
+  var chartData2 = [];
+  var cdi = 1;
+  var cdiPerformance = [];
+  var warrenRF1Performance = [];
+  var warrenMM1Performance = [];
+  var warrenMM2Performance = [];
+  var warrenMM3Performance = [];
+  var warrenMM4Performance = [];
+  var warrenRF1 = 0;
+  var warrenMM1 = 0;
+  var warrenMM2 = 0;
+  var warrenMM3 = 0;
+  var warrenMM4 = 0;
+  var startDate = document.getElementById('startDate').value;
 
+// Build the performance arrays
+  for (var i = 0; i < warrenFundPerformance.chart.label.length; i++) {
+    var monthlyValue = _.findLast(cdiValueChanges, function(item) {
+      return item.date <= warrenFundPerformance.chart.label[i];
+    });
+    // http://minhaseconomias.com.br/blog/investimentos/como-calcular-o-rendimento-de-seu-investimento-em-de-cdi
+    // http://estatisticas.cetip.com.br/astec/di_documentos/metodologia2_i1.htm
+    cdiPerformance.push(
+      (1 + (Math.pow((1+monthlyValue.value/100), 1/252)-1) * 100/100).truncate(16)
+    );
+
+    // Warren
+    warrenRF1Performance.push(warrenFundPerformance.chart.data.FWRF1[i] - warrenFundPerformance.chart.data.FWRF1[i-1] || 0);
+    warrenMM1Performance.push(warrenFundPerformance.chart.data.FWMM1[i] - warrenFundPerformance.chart.data.FWMM1[i-1] || 0);
+    warrenMM2Performance.push(warrenFundPerformance.chart.data.FWMM2[i] - warrenFundPerformance.chart.data.FWMM2[i-1] || 0);
+    warrenMM3Performance.push(warrenFundPerformance.chart.data.FWMM3[i] - warrenFundPerformance.chart.data.FWMM3[i-1] || 0);
+    warrenMM4Performance.push(warrenFundPerformance.chart.data.FWMM4[i] - warrenFundPerformance.chart.data.FWMM4[i-1] || 0);
+  }
+
+  for (var i = 0; i < warrenFundPerformance.chart.label.length; i++) {
+
+    if (warrenFundPerformance.chart.label[i] >= startDate) {
+      chartData2.push({
+        date: warrenFundPerformance.chart.label[i],
+        // +().toFixed() from https://stackoverflow.com/a/12830454/785985
+        FWRF1: +((warrenRF1) * 100),
+        FWMM1: +((warrenMM1) * 100),
+        FWMM2: +((warrenMM2) * 100),
+        FWMM3: +((warrenMM3) * 100),
+        FWMM4: +((warrenMM4) * 100),
+        CDI: (((Math.ceil10(cdi, -8) - 1).truncate(8)) * 100)
+      });
+      if (i < warrenFundPerformance.chart.label.length - 1) {
+        // By using ceil and the -9 parameter, we get the most approximate values yet
+        cdi = Math.ceil10(cdi * (cdiPerformance[i + 1]), -9);
+      }
+      warrenRF1 += warrenRF1Performance[i];
+      warrenMM1 += warrenMM1Performance[i];
+      warrenMM2 += warrenMM2Performance[i];
+      warrenMM3 += warrenMM3Performance[i];
+      warrenMM4 += warrenMM4Performance[i];
+    }
+  }
+
+
+
+  var chart = AmCharts.makeChart( 'chartdiv', {
+    type: 'serial',
+    dataProvider: chartData2,
+    "categoryField": "date",
+    "dataDateFormat": "YYYY-MM-DD",
+    "chartScrollbar": {
+      "enabled": true
+    },
+    'mouseWheelZoomEnabled':true,
+    'chartCursor': {
+      "enabled": true
+      // 'pan': true,
+      // 'valueLineEnabled': true,
+      // 'valueLineBalloonEnabled': true,
+      // 'cursorAlpha':1,
+      // 'cursorColor':'#258cbb',
+      // 'limitToGraph':'g1',
+      // 'valueLineAlpha':0.2,
+      // 'valueZoomable':true
+    },
+    "legend": {
+      "enabled": true,
+      "useGraphSettings": true,
+      // data: [
+      //   {title: 'RF 1'},
+      //   {title: 'MM 1'},
+      //   {title: 'MM 2'},
+      //   {title: 'MM 3'},
+      //   {title: 'MM 4'},
+      //   {title: 'CDI'}
+      // ]
+    },
+    "graphs": [
+      {
+        "bullet": "round",
+        "id": "AmGraph-1",
+        "title": "RF1",
+        "valueField": "FWRF1",
+        'bulletSize': 5,
+        'hideBulletsCount': 50,
+        lineColor: '#ee2e5d',
+        precision: 2,
+        lineThickness: 2
+      },
+      {
+        "bullet": "round",
+        "id": "AmGraph-2",
+        "title": "MM1",
+        "valueField": "FWMM1",
+        'bulletSize': 5,
+        'hideBulletsCount': 50,
+        lineColor: '#ff9001',
+        precision: 2,
+        lineThickness: 2
+      },
+      {
+        "bullet": "round",
+        "id": "AmGraph-3",
+        "title": "MM2",
+        "valueField": "FWMM2",
+        'bulletSize': 5,
+        'hideBulletsCount': 50,
+        lineColor: '#00b563',
+        precision: 2,
+        lineThickness: 2
+      },
+      {
+        "bullet": "round",
+        "id": "AmGraph-4",
+        "title": "MM3",
+        "valueField": "FWMM3",
+        'bulletSize': 5,
+        'hideBulletsCount': 50,
+        lineColor: '#0581ff',
+        precision: 2,
+        lineThickness: 2
+      },
+      {
+        "bullet": "round",
+        "id": "AmGraph-5",
+        "title": "MM4",
+        "valueField": "FWMM4",
+        'bulletSize': 5,
+        'hideBulletsCount': 50,
+        lineColor: '#6960da',
+        precision: 2,
+        lineThickness: 2
+      },
+      {
+        "bullet": "round",
+        "id": "AmGraph-6",
+        "title": "CDI",
+        "valueField": "CDI",
+        'bulletSize': 5,
+        'hideBulletsCount': 50,
+        lineColor: '#000000',
+        precision: 6,
+        lineThickness: 2
+      }
+    ]
+  });
+}
