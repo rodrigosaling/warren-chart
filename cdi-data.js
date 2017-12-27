e1386099b8c019ddf6825cd0246453656adcb135
Rodrigo Saling <rodrigo.saling@e-core.com>
2017-12-27 14:45:35 -0200

    Update CDI current value

--- cdi-data.js
+++ cdi-data.js
@@ -10,11 +10,12 @@ var cdiValueChanges = [
   // {date: '2017-06-01', value: 10.14},
   // {date: '2017-07-27', value: 9.14},
   // {date: '2017-09-08', value: 8.14},
-  // {date: '2017-10-26', value: 7.39}
+  // {date: '2017-10-26', value: 7.39},
+  // {date: '2017-12-07', value: 6.89}
   //-----
   {date: '1998-01-01', value: 0}, // Everything starts at zero
   {date: '2016-10-14', value: 14.13},
-  // Values 1 day after the true change
+  // Values 1 day after the real change
   {date: '2016-10-21', value: 13.88},
   {date: '2016-12-02', value: 13.63},
   {date: '2017-01-13', value: 12.88},
@@ -23,7 +24,8 @@ var cdiValueChanges = [
   {date: '2017-06-02', value: 10.14},
   {date: '2017-07-28', value: 9.14},
   {date: '2017-09-09', value: 8.14},
-  {date: '2017-10-27', value: 7.39}
+  {date: '2017-10-27', value: 7.39},
+  {date: '2017-12-08', value: 6.89}
 ];
 
 // How to calculate CDI: (Math.pow((1+<monthly_value>/100), 1/252)-1)*100
